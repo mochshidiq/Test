3a01c976ea49432fd8a44dacce270bd89082eeae
Muhammad Sidik <muhammad.sidik@binus.edu>
2021-12-28 19:48:37 +0700

    Add project files.

---
+++ IMyInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Test
+{
+    class IMyInterface
+    {
+
+        public string GetWordsCount(string[] inputword)
+        {
+            string hasil = "{ ";
+            foreach (var iw in inputword.Distinct())
+            {
+                var res = from n in inputword
+                          where n == iw
+                          select n;
+                hasil += "{\"" + iw + "\", " + res.Count() + "}, ";
+            }
+            return hasil.Substring(0, hasil.Length - 2) + " }";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMyInterface myInterface = new IMyInterface();
+            var hasil = myInterface.GetWordsCount(new[] { "one", "two", "two", "three", "three", "three", "four", "One" });
+            Console.WriteLine(hasil);
+            Console.ReadKey();
+        }
+    }
+}
