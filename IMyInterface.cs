0cda5d372dc3bf488313f21379ce66911ec77d0e
Muhammad Sidik <muhammad.sidik@binus.edu>
2022-01-06 22:41:18 +0700

    - update games dice to the turn

--- IMyInterface.cs
+++ IMyInterface.cs
@@ -7,18 +7,74 @@ namespace Test
 {
     class IMyInterface
     {
-
-        public string GetWordsCount(string[] inputword)
+        public class playerdata
         {
-            string hasil = "{ ";
-            foreach (var iw in inputword.Distinct())
+            public string playername { get; set; }
+            public int score { get; set; }
+            public List<int> dice { get; set; }
+        }
+
+        public void GetWinner(int player, int dicecount)
+        {
+            List<playerdata> playerdatalist = new List<playerdata>();
+            for(int i = 1; i < player + 1; i++)
             {
-                var res = from n in inputword
-                          where n == iw
-                          select n;
-                hasil += "{\"" + iw + "\", " + res.Count() + "}, ";
+                List<int> dice2 = new List<int>();
+                int dicecount2 = dicecount;
+                while (dicecount2 != 0)
+                {
+                    dice2.Add(7);
+                }
+                playerdatalist.Add(new playerdata{
+                    playername = "Pemain #" + i,
+                    score = 0,
+                    dice = dice2
+                });
             }
-            return hasil.Substring(0, hasil.Length - 2) + " }";
+
+            int playercount = player, iterasi = 0;
+            Random rd = new Random();
+            
+            while (playercount != 1)
+            {
+                iterasi++;
+                Console.WriteLine("Giliran " + iterasi + " lempar dadu:");
+
+                foreach (var playersingle in playerdatalist)
+                {
+
+                    Console.Write(playersingle.playername + " (" + playersingle.score + "): ");
+
+                    dicecount = iterasi != 1 ? playersingle.dice.Count() : dicecount;
+                    for (int i = 0; i < dicecount; i++)
+                    {
+                        if (playersingle.dice[i] != 0)
+                        {
+                            int rand_num = rd.Next(1, 6);
+                            playersingle.dice[i] = rand_num;
+                            if (i == 0)
+                                Console.Write(rand_num);
+                            else
+                                Console.Write("," + rand_num);
+                            if (rand_num == 6)
+                            {
+                                playersingle.score++;
+                                playersingle.dice[i] = 0;
+                            }
+                            else if (rand_num == 1)
+                            {                                
+                                playersingle.dice[i] = 0;
+
+                            }
+                        }
+                    }
+                    
+                    Console.WriteLine();
+                }
+            }
+
+
+            Console.WriteLine("The winner is : " + string.Join(',', playerdatalist.Where(x => x.score == playerdatalist.Max(x => x.score)).Select(x => x.playername).ToList()));
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ namespace Test
         {
             IMyInterface myInterface = new IMyInterface();
             //string[] inputan = new[] { "one", "two", "two", "three", "three", "three", "four", "One" };
-            Console.Write("Input your words (separate with space) !!! : ");
-            string[] inputan = Console.ReadLine().TrimEnd().Split(' ').ToArray();
-            var hasil = myInterface.GetWordsCount(inputan);
-            Console.WriteLine(hasil);
+            Console.Write("Input your player count !!! : ");
+            int player = Int32.Parse(Console.ReadLine().Trim());
+            Console.Write("Input your dice count (separate with space) !!! : ");
+            int dice = Int32.Parse(Console.ReadLine().Trim());
+            myInterface.GetWinner(player, dice);
+            
             Console.ReadKey();
         }
     }
